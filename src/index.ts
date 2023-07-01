424bf2e9c5815672cff815311f6d2a7f1f3a9d06
Stas Leshuk <leshuksu@gmail.com>
2023-07-01 17:52:40 +0300

    started company

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { Employee } from "./User";
+import { Company } from "./Company";
 
 const employee = new Employee();
+const company = new Company();
+
 console.log(employee);
+console.log(company);
