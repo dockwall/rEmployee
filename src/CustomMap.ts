21c0b60ee28a6f094fc6a6e069bcc60e865feeb1
Stanislav Leshuk <leshuksu@gmail.com>
2023-07-02 20:16:14 +0300

    Added custom map methods and Markers render (#1)
    * added methods (bad, duplicating realisation)
    
    * refactored
    
    * updated default zoom value
    
    * deleted unused imports

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,10 @@
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -5,12 +12,22 @@ export class CustomMap {
     this.googleMap = new google.maps.Map(
       document.getElementById(htmlElementID) as HTMLElement,
       {
-        zoom: 1,
+        zoom: 2,
         center: {
           lat: 0,
           lng: 0,
         },
       }
     );
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import { Employee } from "./User";
 import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
 
+const customMap = new CustomMap("map");
+
 const employee = new Employee();
 const company = new Company();
-const customMap = new CustomMap("map");
 
 console.log(employee);
 console.log(company);
 console.log(customMap);
+
+customMap.addMarker(employee);
+customMap.addMarker(company);
