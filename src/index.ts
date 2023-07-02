cb906ce22e5d2beda68a054180b0b6bd2bb63d19
Stas Leshuk <leshuksu@gmail.com>
2023-07-02 15:19:42 +0300

    Added map instance and played with it

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,14 @@ const company = new Company();
 
 console.log(employee);
 console.log(company);
+
+const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
+
+// let i = 1;
+// setInterval(() => map.setZoom(i++), 1000);
